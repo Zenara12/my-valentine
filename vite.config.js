b6314d64265a934a9823309b2f95e0c52ce7fee8
Noli Pernes <nolipernes@gmail.com>
2026-02-19 15:17:18 +0800

    fix: remove build in vite

--- vite.config.js
+++ vite.config.js
@@ -10,11 +10,6 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  // root: 'src',
-  build: {
-    outDir: '../dist', // Output to 'dist' directory outside of 'src'
-    emptyOutDir: true, // Clears the output directory before build
-  },
   resolve: {
     alias: {
       // Optional: You can also set up an alias for '@' to point to 'src'
